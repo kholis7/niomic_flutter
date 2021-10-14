525c03485586cc3cf7b6f78083efa2669e01aa1b
Kholis_7 <kholissetia17@gmail.com>
2021-10-14 22:06:45 +0700

    Project Pertama Flutter
    Project Flutter Niomic First APP

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Selamat Datang Flutter"),
+      ),
+    );
+  }
+}
